744db9ae46ca06b6fd59415e41f0e070f91eb139
Raeski <gustavoraeski@outlook.com>
2021-04-11 17:00:23 -0300

    initial commit

---
+++ src/main/java/com/gps/pontosdeinteresse/PontosdeinteresseApplication.java
@@ -0,0 +1,13 @@
+package com.gps.pontosdeinteresse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosdeinteresseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontosdeinteresseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gps/pontosdeinteresse/PontosdeinteresseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gps.pontosdeinteresse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosdeinteresseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
