ca886e935ee17da2d7f4b0a9e07f2e2137a31df8
Raeski <gustavoraeski@outlook.com>
2021-04-11 22:57:34 -0300

    creating entity and connection to database

--- src/main/java/com/gps/pontosdeinteresse/PointOfInterest.java
+++ src/main/java/com/gps/pontosdeinteresse/PointOfInterest.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PontosdeinteresseApplication {
+public class PointOfInterest {
 
     public static void main(String[] args) {
-        SpringApplication.run(PontosdeinteresseApplication.class, args);
+        SpringApplication.run(PointOfInterest.class, args);
     }
 
 }
---
+++ src/main/java/com/gps/pontosdeinteresse/model/Pois.java
@@ -0,0 +1,30 @@
+package com.gps.pontosdeinteresse.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pois {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long cordX;
+
+    private Long cordY;
+
+}
